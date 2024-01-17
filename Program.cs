bd7b0de75344030d893cf0fd6915f4d6e4035443
Dch <davidchazanas@gmail.com>
2024-01-17 16:47:55 -0300

    add swagger

--- Program.cs
+++ Program.cs
@@ -16,6 +16,10 @@ builder.Services.AddScoped<IService, CatService>();
 builder.Services.AddDbContext<CatContext>(opt =>
     opt.UseInMemoryDatabase("CatDatabase"));
 
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,6 +27,9 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+} else {
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
